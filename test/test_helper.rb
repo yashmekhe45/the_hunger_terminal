55fb4fec1af8d2777e2afa6d18ce31a660b12a08
Sakshi Parakh <sakshi2894@gmail.com>
2017-02-08 17:13:54 +0530

    Re arranged order of simplecov configuration [#21]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,13 @@
 ENV["RAILS_ENV"] = "test"
 
+require 'simplecov'
 require "codeclimate-test-reporter"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
-require 'simplecov'
 
-CodeClimate::TestReporter.start
 SimpleCov.start
+CodeClimate::TestReporter.start
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
 # require "minitest/rails/capybara"

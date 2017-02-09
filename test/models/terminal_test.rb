2ba7f7529c0446835aa79326ac63bdbda7cdff36
imAkshay26 <akshay@joshsoftware.com>
2017-02-09 13:56:34 +0530

    test cases written for terminals [#9]

--- test/models/terminal_test.rb
+++ test/models/terminal_test.rb
@@ -2,12 +2,29 @@ require "test_helper"
 
 class TerminalTest < ActiveSupport::TestCase
   
-  def test_should_not_save_terminal_without_name
+  test "should not save blank record" do
     terminal = Terminal.new
-    assert_not terminal.save
+    assert_not terminal.valid?
   end
 
-  def test_should_not_save_terminal_without_landline
-    
+  test "should not save terminal without landline" do
+    terminal =  FactoryGirl.build(:terminal ,:landline =>  nil)
+    terminal.valid?
+    assert_not_empty terminal.errors[:landline]    
+  end
+
+  test "should not save terminal without name" do
+    terminal = FactoryGirl.build(:terminal,:name => nil)
+    terminal.valid?
+    assert_not_empty terminal.errors[:name]
+  end
+
+  test "should not duplicate landline" do
+    terminal = Terminal.create(:name=>"rolls mania",:landline=>"022-12365478")
+    print terminal
+    duplicate_rec = terminal.dup
+    print duplicate_rec
+    duplicate_rec.valid?
+    assert_not_empty duplicate_rec.errors[:landline]
   end
 end

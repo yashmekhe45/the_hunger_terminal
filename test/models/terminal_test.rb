8029d96de9f83239de63729ed79fc59da29dd849
imAkshay26 <akshay@joshsoftware.com>
2017-02-10 12:26:23 +0530

    landline no pattern should 022-12365498 [#9]

--- test/models/terminal_test.rb
+++ test/models/terminal_test.rb
@@ -1,12 +1,5 @@
 require "test_helper"
-
 class TerminalTest < ActiveSupport::TestCase
-  
-  test "should not save blank record" do
-    terminal = Terminal.new
-    assert_not terminal.valid?
-  end
-
   test "should not save terminal without landline" do
     terminal =  FactoryGirl.build(:terminal ,:landline =>  nil)
     terminal.valid?
@@ -21,10 +14,20 @@ class TerminalTest < ActiveSupport::TestCase
 
   test "should not duplicate landline" do
     terminal = Terminal.create(:name=>"rolls mania",:landline=>"022-12365478")
-    print terminal
     duplicate_rec = terminal.dup
-    print duplicate_rec
     duplicate_rec.valid?
     assert_not_empty duplicate_rec.errors[:landline]
+  end
+
+  test "landline no should have length 12" do
+    terminal = Terminal.create(:name=>"kfc",:landline=>"012-1023")
+    terminal.valid?
+    assert_not_empty terminal.errors[:landline]
+  end
+
+  test "landline no should have only [0-9] and dash" do
+    terminal = FactoryGirl.build(:terminal,:landline => "akshaykakade")
+    terminal.valid?
+    assert_not_empty terminal.errors[:landline]
   end
 end

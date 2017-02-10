a0e739875d58bba32fb2ef39241d3cad2546f1c7
imAkshay26 <akshay@joshsoftware.com>
2017-02-10 13:06:43 +0530

    landline validator code improved

--- app/models/landline_validator.rb
+++ app/models/landline_validator.rb
@@ -1,7 +1,10 @@
 class LandlineValidator < ActiveModel::Validator
   def validate(record)
-    if record[:landline].match(/^[0-9]\d{2,4}-\d{6,8}$/) == nil
-      record.errors[:landline] << 'please enter valid landline no ex.022-203165'
+    if (record[:landline] == nil)
+      return
+    end
+    if (record[:landline].match(/\A(0\d{2}-\d{8})|(0\d{3}-\d{7})|(0\d{4}-\d{6})\z/) == nil)
+      record.errors[:landline] << 'please enter valid landline no eg.022-20316523'
     end
   end
 end

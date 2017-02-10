82cf122bd37b8f96cbe14d936e6fbe335193de1f
kiran123 <kiranbdesh123@gmail.com>
2017-02-10 10:51:40 +0530

    test cases are written for company model

--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -9,25 +9,48 @@ class CompanyTest < ActiveSupport::TestCase
   #   assert_not company.valid?, "saved company record without any attribute"
   # end
 
-  test "name must be present" do
+  test "name should be present" do
     company_obj = FactoryGirl.build(:company,:name => nil)
     company_obj.valid?
     assert_not_empty company_obj.errors[:name]
   end
 
-  test "landline must be present" do
+  test "landline should be present" do
     company_obj = FactoryGirl.build(:company,:landline => nil)
     company_obj.valid?
-    assert_not_empty company_obj.errors[:landline]
+    assert company_obj.errors[:landline].include?("can't be blank")
   end
 
-  #whether the dummy record is going to be stored in DB or not
-  test "landline must be unique" do
+  
+  test "landline should be unique" do
     company = FactoryGirl.build(:company)
     duplicate_record =company.dup
     company.save(validate: false)
     duplicate_record.valid?
     # assert duplicate_record.errors
-    assert_equal duplicate_record.errors[:landline], ["has already been taken"]
+    assert duplicate_record.errors[:landline].include?("has already been taken")
   end
+
+
+  test "name should be unique under case insensitive scope" do
+    company = FactoryGirl.build(:company)
+    duplicate_record =company.dup
+    duplicate_record[:name] = company[:name].upcase
+    company.save(validate: false)
+    duplicate_record.valid?
+    assert duplicate_record.errors[:name].include?("has already been taken")
+  end
+
+
+  test "landline should be valid indian landline number" do
+    company = FactoryGirl.build(:company, :landline => 'k')
+    company.valid?
+    assert company.errors[:landline].include?("Please enter valid landline number!")
+  end
+
+  test "landline should have 12 characters " do
+    company = FactoryGirl.build(:company)
+    company.valid?
+    assert company.errors[:landline].include?("is the wrong length (should be 12 characters)")
+  end 
 end

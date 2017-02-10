4cc3345754b313386581ddb2e6aac7ce441c47f6
kiran123 <kiranbdesh123@gmail.com>
2017-02-10 10:53:20 +0530

    test cases are written for company model

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,13 @@
 ENV["RAILS_ENV"] = "test"
 
-require 'simplecov'
-require "codeclimate-test-reporter"
+# require "codeclimate-test-reporter"
+# CodeClimate::TestReporter.start
+require 'simplecov' 
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
+SimpleCov.start 
 
-SimpleCov.start
-CodeClimate::TestReporter.start
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
 # require "minitest/rails/capybara"

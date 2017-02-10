804d62db685fa73a1ca2a09be7b170247fdcbee7
kiran123 <kiranbdesh123@gmail.com>
2017-02-10 17:15:50 +0530

    added relation company has one address

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,17 +1,18 @@
 class Company < ApplicationRecord
   include ActiveModel::Validations
 
-  
+  validates_with LandlineValidator
   validates :name, :landline, presence: true
   validates :name, uniqueness:{case_sensitive: false}
   validates :landline, uniqueness: true
-  validates_with LandlineValidator
+  
   validates :landline, length: {is: 12}
 
   has_one :address, dependent: :destroy, as: :location
 
   before_validation :remove_space
   def remove_space
+    #squish method is not for nil classes
     if(self.name == nil)
       return
     end

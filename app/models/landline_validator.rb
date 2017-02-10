a5b05a8f40c6a8e939b3f874267aebbaad356ec2
kiran123 <kiranbdesh123@gmail.com>
2017-02-10 10:48:14 +0530

    custom validations on landline number

--- app/models/landline_validator.rb
+++ app/models/landline_validator.rb
@@ -1,6 +1,10 @@
 class LandlineValidator < ActiveModel::Validator
   def validate(record)
-    if(record[:landline].match(/^[0-9]\d{2,4}-\d{6,8}$/)) == nil
+    # match method is not for nil classes
+    if (record[:landline] == nil)
+      return
+    end
+    if (record[:landline].match(/^[0](\d{2}-\d{8})|(\d{3}-\d{7})|(\d{4}-\d{6})$/)) == nil
       record.errors[:landline] << 'Please enter valid landline number!'
     end
   end

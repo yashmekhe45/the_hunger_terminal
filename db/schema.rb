c212007f50c0b86321e93bd991454432e901ad59
kiran123 <kiranbdesh123@gmail.com>
2017-02-10 17:23:53 +0530

    Polymorphic relation "location" is added for address AND foreign key relation between address and company is removed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208162530) do
+ActiveRecord::Schema.define(version: 20170210081825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,10 +21,11 @@ ActiveRecord::Schema.define(version: 20170208162530) do
     t.string   "locality"
     t.string   "city"
     t.string   "state"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "company_id"
-    t.index ["company_id"], name: "index_addresses_on_company_id", using: :btree
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "location_type"
+    t.integer  "location_id"
+    t.index ["location_type", "location_id"], name: "index_addresses_on_location_type_and_location_id", using: :btree
   end
 
   create_table "companies", force: :cascade do |t|
@@ -59,5 +60,4 @@ ActiveRecord::Schema.define(version: 20170208162530) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "addresses", "companies"
 end

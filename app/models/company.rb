13c362f49400716f8a0ced4d96e3b907e7fc9d80
kiran123 <kiranbdesh123@gmail.com>
2017-02-10 10:46:53 +0530

    validations are added in Company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,11 +1,21 @@
 class Company < ApplicationRecord
-  # include ActiveModel::Validations
+  include ActiveModel::Validations
 
-  # validates_with LandlineValidator
-  # validates :name, :landline, presence: true
-  # # validates :name, uniqueness:{case_sensitive: false}
-  # validates :landline, uniqueness: true
+  
+  validates :name, :landline, presence: true
+  validates :name, uniqueness:{case_sensitive: false}
+  validates :landline, uniqueness: true
+  validates_with LandlineValidator
+  validates :landline, length: {is: 12}
 
-  # has_one :address, dependent: :destroy
+  has_one :address, dependent: :destroy, as: :location
+
+  before_validation :remove_space
+  def remove_space
+    if(self.name == nil)
+      return
+    end
+    self.name = name.squish
+  end
   # has_many :users, as :employees, dependent: :destroy
 end

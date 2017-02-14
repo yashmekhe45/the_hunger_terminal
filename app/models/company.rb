5aae724585e4713e1ebbf992c52caa0463532d0d
kiran123 <kiranbdesh123@gmail.com>
2017-02-14 19:13:33 +0530

    validations for address and user are inserted[#6]

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,11 +5,12 @@ class Company < ApplicationRecord
   validates :name, :landline, presence: true
   validates :name, uniqueness:{case_sensitive: false}
   validates :landline, uniqueness: true
-  
   validates :landline, length: {is: 12}
+  validates :address, presence: true
+  validates :employees, presence: true
 
   has_one :address, dependent: :destroy, as: :location
-
+  has_many :employees , class_name: "User", dependent: :destroy
   before_validation :remove_space
   def remove_space
     #squish method is not for nil classes
@@ -18,5 +19,5 @@ class Company < ApplicationRecord
     end
     self.name = name.squish
   end
-  has_many :users, as: :employees, dependent: :destroy
+  
 end

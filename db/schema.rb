07e2e2b1ae8bbb062d79c704d654932b5b508591
tejaswini-gambhire <gambhire.tejaswini30@gmail.com>
2017-02-14 11:44:47 +0530

    modified schema.rb after migrations[#7]

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208162530) do
+ActiveRecord::Schema.define(version: 20170213104958) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
   create_table "addresses", force: :cascade do |t|
     t.string   "house_no"
     t.integer  "pincode"
@@ -77,8 +79,6 @@ ActiveRecord::Schema.define(version: 20170208162530) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-
-  add_foreign_key "users", "companies"
-
   add_foreign_key "menu_items", "terminals"
+  add_foreign_key "users", "companies"
 end

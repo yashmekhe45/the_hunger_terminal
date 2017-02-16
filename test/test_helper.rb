0a70e7ac4baefecbc7709cc04561f545f1f3595c
imAkshay26 <akshay@joshsoftware.com>
2017-02-16 11:13:18 +0530

    modified for to run the test cases on terminal [#9]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,6 +17,7 @@ CodeClimate::TestReporter.start
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  include FactoryGirl::Syntax::Methods
   fixtures :all
   # Add more helper methods to be used by all tests here...
 end

c3f9019d696e662df01829f4baa5488c7fcb3270
imAkshay26 <akshay@joshsoftware.com>
2017-02-20 11:17:58 +0530

    added test cases for csv upload [#9]

--- test/controllers/terminals_controller_test.rb
+++ test/controllers/terminals_controller_test.rb
@@ -44,10 +44,44 @@ class TerminalsControllerTest < ActionDispatch::IntegrationTest
   test "should update terminal" do
     @terminal2 = Terminal.create(:name=>"nksdjvfn",:landline=>"0123-1210446",)
     @terminal = Terminal.create(:name=>"nksdrtjvn",:landline=>"0123-1015446")
-    @menuitem = MenuItem.create(name:"fegeg",price:1233,veg:true)
-    put terminal_url(@terminal), params: { terminal: { name:@terminal2.name,landline:@terminal.landline,
+    @menuitem = @terminal.menu_items.create(name:"fegeg",price:1233,veg:true)
+    put terminal_url(@terminal.id), params: { terminal: { name:@terminal2.name,landline:@terminal.landline,
       "menu_items_attributes"=>{"0"=>{"name"=>@menuitem.name, "price"=>@menuitem.price, "veg"=>"1", "_destroy"=>"false", 
       "id"=>@menuitem.id}}}}
     assert_equal @terminal2.name ,@terminal.name
+  end
+
+  test "should successfully import csv"
+    csv_rows = <<-eos
+name,price,veg
+beer,224,true
+Name3,324,true
+eos
+    file = Tempfile.new('new_users.csv')
+    file.write(csv_rows)
+    file.rewind
+    @terminal = Terminal.create(name:"kjnf",landline:"0125-1563156")
+    assert_difference "MenuItem.count", 2 do
+      post import_terminal_path(@terminal.id), :file => Rack::Test::UploadedFile.new(file, 'text/csv')
+    end
+
+    assert_redirected_to terminal_path
+    assert_equal "all menu items added", flash[:success]
+  end
+
+  test "should gives invalid csv records"
+    csv_rows = <<-eos
+name,price,veg
+beer,-44,true
+Name3,-78,true
+eos
+    file = Tempfile.new('new_users.csv')
+    file.write(csv_rows)
+    file.rewind
+    @terminal = Terminal.create(name:"kjnf",landline:"0125-1563156")
+    assert_difference "MenuItem.count", 0 do
+      post import_terminal_path(@terminal.id), :file => Rack::Test::UploadedFile.new(file, 'text/csv')
+    end
+    assert_equal "You have invalid records.", flash[:alert]
   end
 end

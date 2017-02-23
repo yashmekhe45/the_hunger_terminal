522a87f331a6fab90d609f6d6892a80d95537d98
tejaswini-gambhire <gambhire.tejaswini30@gmail.com>
2017-02-23 13:36:11 +0530

    after sign in path for company admin added[#18]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    if current_user.role == "company_admin"
+      terminals_path
+    end
+  end
 end

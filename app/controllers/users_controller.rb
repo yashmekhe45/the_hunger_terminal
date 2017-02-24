80267509ea168210641d198816866f6eb4528324
kiran123 <kiranbdesh123@gmail.com>
2017-02-24 20:41:37 +0530

    Send invitation to employee using confirmable[#14]
    Sending inviattion using devise confirmable is done. Downloading file
    having invalid records and sharing of password is remaining

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,14 @@ class UsersController < ApplicationController
 
   before_action :load_user , only:[:show, :edit, :update]
 
+  before_action :load_company, only: [:index, :import]
+
   def new
     @company = Company.new
     @user = @company.employees.build
   end
 
   def index
-    @company = Company.find(params[:company_id])
     @users = @company.employees.where(role: "employee").order(:created_at).page(params[:page]).per(2)  
   end
   
@@ -37,6 +38,50 @@ class UsersController < ApplicationController
     end
   end
 
+
+
+  def import
+    valid_header =  ["name","email","mobile_number"]
+    @company = Company.find(params[:company_id])
+    @check_user = User.new
+    if params[:file].content_type == 'text/csv'
+      users_csv = File.open(params[:file].path)
+      users_data = CSV.parse(users_csv,headers:true)
+      if users_data.headers == valid_header
+        users_data.each do |user_row|
+          next if user_row.to_a == valid_header
+          user_hash = user_row.to_h
+          user = @company.employees.build(name: user_hash["name"],email: user_hash["email"], 
+            mobile_number: user_hash["mobile_number"],is_active: true, role: "employee",
+            password: Devise.friendly_token.first(8))
+
+          if user.valid?
+            user.save
+          else
+            CSV.open($INVALID_USER_CSV="public/#{@company.name}-invalid-recrords-#{Date.today}.csv","a+") do |csv|
+              user_row << user.errors.messages
+              csv << user_row
+            end
+            @check_user = user
+          end
+        end
+        if @check_user.valid?
+          flash[:success] = "all users added through csv data"
+          redirect_to company_users_path(@company_id)
+        else
+          flash[:notice] = "You have some invalid records.Correct it and upload it again"
+        end
+      else
+        flash[:error] = "invlaid headres in your csv."
+      end
+    else
+      flash[:error] = "invalid type of file please upload csv with valid headers"
+    end
+  end
+
+  
+
+
   def search
     search_value = params[:search_value]
     p search_value
@@ -49,7 +94,6 @@ class UsersController < ApplicationController
     end
   end
 
-
   private
 
   def user_params
@@ -59,6 +103,10 @@ class UsersController < ApplicationController
   def load_user
     @company = Company.find(params[:company_id])
     @user = @company.employees.find(params[:id])
+  end
+
+  def load_company
+    @company = Company.find(params[:company_id])  
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
       get 'search', :on => :collection
       collection { post :import }
     end
+
   end
 
   resources :terminals do

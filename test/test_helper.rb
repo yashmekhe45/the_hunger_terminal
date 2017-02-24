87cdcd7e9a76e2e0dad2afc54eaeb20f076923a5
tejaswini-gambhire <gambhire.tejaswini30@gmail.com>
2017-02-24 17:28:55 +0530

    configures database cleaner[#18]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,7 +9,7 @@ CodeClimate::TestReporter.start
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
-
+require "database_cleaner"
 
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
@@ -22,5 +22,8 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   include FactoryGirl::Syntax::Methods
   fixtures :all
+  DatabaseCleaner.strategy = :truncation
+  DatabaseCleaner.start
+  DatabaseCleaner.clean
   # Add more helper methods to be used by all tests here...
 end

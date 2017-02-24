d6556d6d40b696b3780b883e01212d8fcefacd97
imAkshay26 <akshay@joshsoftware.com>
2017-02-24 18:53:17 +0530

    csv file type added [#13]

--- app/controllers/terminals_controller.rb
+++ app/controllers/terminals_controller.rb
@@ -39,12 +39,10 @@ class TerminalsController < ApplicationController
     end
   end
 
-  
-
   def index
     if params[:search].present?
       search_type = params[:search]
-      @terminal_search = Terminal.where(["name LIKE ?", "%#{params[:search]}%"])
+      @terminal_search = Terminal.where(["name LIKE ?", "%#{params[:search]}%"]).page(params[:page]).per(5)
       if @terminal_search.empty?
         flash[:notice] = "Vendor not present."
       end
@@ -58,7 +56,9 @@ class TerminalsController < ApplicationController
   end
 
   def show
-    if params[:id] == "download"
+    if params[:id] == "selected"
+      selected 
+    elsif params[:id] == "download"
       download
     else  
       @terminal = Terminal.find(params[:id])
@@ -68,21 +68,20 @@ class TerminalsController < ApplicationController
 
   def update
     @terminal = Terminal.find(params[:id])
-    if @terminal.update_attributes(terminal_menu_param)
-      flash[:success] = "terminal updated"
-      if params[:terminal][:file].nil?
-        if @terminal.menu_items.empty?
-          flash[:alert] = "you deleted all items so your terminal is deleted"
-          @terminal.destroy
-          redirect_to terminals_path
-        else
+    if params[:terminal].has_key?("is_active") && params[:terminal].size==1
+      @terminal.update_attributes(terminal_menu_param)  
+      redirect_to :back
+    else
+      if @terminal.update_attributes(terminal_menu_param)
+        flash[:success] = "terminal updated"
+        if params[:terminal][:file].nil?
           redirect_to terminal_path
+        else
+          import(params[:id])
         end
       else
-        import(params[:id])
+        render :edit
       end
-    else
-      render :edit
     end
   end
 
@@ -94,33 +93,38 @@ class TerminalsController < ApplicationController
 
   def import(object_id)
     @terminal = Terminal.find(object_id)
-    @menu_item_errors = MenuItem.new(name:"cxkvbivbad",price:2424,veg:true,terminal_id:@terminal.id)
+    @menu_item_errors = MenuItem.new
     $INVALID_RECORD_CSV = nil
     csv_file = File.open(params[:terminal][:file].path)
-    menu_items = CSV.parse(csv_file, headers: true)
-    if menu_items.headers == ["name","price","veg"]
-      menu_items.each do |row|
-        next if row.to_a == ["name","price","veg"]
-        @menu_item = @terminal.menu_items.build(row.to_h)
-        if @menu_item.valid?
-          @menu_item.save
-        else
-          CSV.open($INVALID_RECORD_CSV="public/#{@terminal.name}-invalid_records-#{Date.today}.csv", "a+") do |csv|
-            row << @menu_item.errors.messages.to_a
-            csv << row
+    if params[:terminal][:file].content_type == "text/csv"  
+      menu_items = CSV.parse(csv_file, headers: true)
+      if menu_items.headers == ["name","price","veg"]
+        menu_items.each do |row|
+          next if row.to_a == ["name","price","veg"]
+          @menu_item = @terminal.menu_items.build(row.to_h)
+          if @menu_item.valid?
+            @menu_item.save
+          else
+            CSV.open($INVALID_RECORD_CSV="public/#{@terminal.name}-invalid_records-#{Date.today}.csv", "a+") do |csv|
+              row << @menu_item.errors.messages.to_a
+              csv << row
+            end
+            @menu_item_errors = @menu_item
           end
-          @menu_item_errors = @menu_item
         end
-      end
-      if @menu_item_errors.valid?
-        flash[:success] = "all menu items added using csv file"
-        redirect_to terminal_path(@terminal.id)
+        if @menu_item_errors.valid?       
+          flash[:success] = "all menu items added using csv file"
+          redirect_to terminal_path(@terminal.id)
+        else
+          flash[:alert] = "You have invalid some records.Correct it and upload again." 
+          redirect_to terminal_path(@terminal.id)
+        end     
       else
-        flash[:alert] = "You have invalid some records.Correct it and upload again." 
-        redirect_to terminal_path(@terminal.id)
-      end     
+        flash[:error] = "You have invlaid csv please upload csv with valid headers."
+        redirect_to terminals_path
+      end
     else
-      flash[:error] = "You have invlaid csv please upload csv with valid headers."
+      flash[:alert] = "Invalid type of file.Please upload csv file"
       redirect_to terminals_path
     end   
   end 
@@ -132,7 +136,7 @@ class TerminalsController < ApplicationController
   end
 
   def terminal_menu_param
-    params.require(:terminal).permit(:name,:landline, menu_items_attributes: [:id, :name, :veg, :price, :_destroy])
+    params.require(:terminal).permit(:name,:landline, :is_active,  menu_items_attributes: [:id, :name, :veg, :price, :_destroy])
   end
 
   def download

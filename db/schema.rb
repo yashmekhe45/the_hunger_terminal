f0f37604a88832cdac747c0fd76f8ba4cbaabdca
imAkshay26 <akshay@joshsoftware.com>
2017-02-24 19:07:42 +0530

    is_active field added for terminal [#13]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213104958) do
+ActiveRecord::Schema.define(version: 20170224050404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20170213104958) do
     t.string   "landline"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "is_active"
   end
 
   create_table "users", force: :cascade do |t|

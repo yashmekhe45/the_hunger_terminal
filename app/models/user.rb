a10366cc17814dd8ba9a9c0bf7cff7172b5f6aea
tejaswini-gambhire <gambhire.tejaswini30@gmail.com>
2017-02-24 17:32:14 +0530

    removed unnecessary lines from user model[#18]

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :name, :mobile_number, :role, :email, presence: true
   validates :mobile_number, length: {is: 13}
   validates :role, inclusion: {in: ['super_admin', 'company_admin', 'employee']}
-  validates_presence_of :company_id , :if => :is_employee? 
+  validates_presence_of :company_id, :if => :is_employee? 
   validates :is_active, inclusion: {in: [true, false, 't','f', 'true','false']}, :unless => :is_super_admin?
 
   belongs_to :company
@@ -38,11 +38,10 @@ class User < ApplicationRecord
   end
 
   def not_a_string
-    p self.is_active_before_type_cast
     if [true,false,'t', 'f', 'true','false',1,0].include?(self.is_active_before_type_cast) 
-        return true
+      return true
     else
-      p self.errors[:is_active] << 'This must be true or false.' 
+      self.errors[:is_active] << 'This must be true or false.' 
       return false
     end
   end

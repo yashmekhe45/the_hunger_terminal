6a91c6e6466a9880b95585a77396bb77ec0d1f12
[Shivamkumarsingh] <shivam1995k@gmail.com>
2017-02-25 23:37:06 +0530

    edited mail interceptor

--- config/initializers/development_mail_interceptor.rb
+++ config/initializers/development_mail_interceptor.rb
@@ -1,6 +1,6 @@
 class DevelopmentMailInterceptor 
   def self.delivering_email(message)
     # message.subject = ""  
-    message.to = ["akshay.25kakade@gmail.com"]
+    message.to = ["shivam1995k@gmail.com"]
   end
 end

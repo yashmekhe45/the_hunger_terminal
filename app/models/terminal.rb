3c1caffe71b4dffe6d2c6977be265fffc9468253
[Shivamkumarsingh] <shivam1995k@gmail.com>
2017-02-27 13:21:44 +0530

    syntax correction

--- app/models/terminal.rb
+++ app/models/terminal.rb
@@ -1,4 +1,4 @@
--class Terminal < ApplicationRecord
+class Terminal < ApplicationRecord
   validates_with LandlineValidator
   validates :name, :landline ,presence: true
   validates :landline ,uniqueness: true

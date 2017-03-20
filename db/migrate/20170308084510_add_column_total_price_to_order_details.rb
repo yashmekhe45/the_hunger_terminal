5f4cbe08ebe894d605c1b3ef797563e2d58167e0
tejaswini-gambhire <gambhire.tejaswini30@gmail.com>
2017-03-20 17:56:44 +0530

    Removes total_price from order_details

--- db/migrate/20170308084510_add_column_total_price_to_order_details.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnTotalPriceToOrderDetails < ActiveRecord::Migration[5.0]
-  def change
-    add_column :order_details, :total_price, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308084510) do
+ActiveRecord::Schema.define(version: 20170224072300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 20170308084510) do
     t.boolean  "veg"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
-    t.integer  "total_price"
     t.index ["order_id"], name: "index_order_details_on_order_id", using: :btree
     t.index ["terminal_id"], name: "index_order_details_on_terminal_id", using: :btree
   end

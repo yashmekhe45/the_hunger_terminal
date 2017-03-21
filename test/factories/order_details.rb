a3c77ca560e43f2a91efb13b0fb88a97414e9339
tejaswini-gambhire <gambhire.tejaswini30@gmail.com>
2017-03-21 18:58:30 +0530

    Modifies order_details factory to add status

--- test/factories/order_details.rb
+++ test/factories/order_details.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
     menu_item_name "menu_name"
     price { Faker::Number.positive }
     quantity { Faker::Number.positive }
+    status {"available"}
     
     association :order
     association :menu_item

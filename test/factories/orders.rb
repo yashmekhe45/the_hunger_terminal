e42377d5fc6af6cf9f86d046da692377442ea3b1
tejaswini-gambhire <gambhire.tejaswini30@gmail.com>
2017-03-21 19:11:09 +0530

    Modifies order factory with status and subsidy

--- test/factories/orders.rb
+++ test/factories/orders.rb
@@ -2,9 +2,12 @@ FactoryGirl.define do
   factory :order do
     date { Faker::Date.between(Date.today,1.days.from_now) }
     total_cost { Faker::Number.positive }
+    status {"pending"}
+    subsidy { Faker::Number.decimal(2,3) }
 
     association :user
     association :company
+    association :terminal
 
     after(:build) do |order|
       order.order_details_attributes = [FactoryGirl.attributes_for(:order_detail)]

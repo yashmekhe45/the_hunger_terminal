ef47001c9d065ceab31eb17be8a47ea74a755e26
tejaswini-gambhire <gambhire.tejaswini30@gmail.com>
2017-03-24 14:42:48 +0530

    Resolves issue with seeds.rb[#69]

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,16 @@
 
-company1 = Company.find_or_initialize_by(name: "Dummy software", landline: "0235-1923222")
-company1.employees.find_or_initialize_by(name: 'Tejaswini Gambhire', mobile_number: "+918830223850", email: "tejaswini@joshsoftware.com",
-                      password: "tejaswini",confirmed_at: Time.now)
+company1 = Company.find_or_initialize_by(name: "Dummy software", landline: "0235192322")
+u = company1.employees.find_or_initialize_by(name: 'Tejaswini Gambhire', mobile_number: "8830224850", email: "tejaswini@test.com",
+  role: "company_admin", is_active: true)
+u.password = "tejaswini"
+u.confirmed_at = Time.now if !u.confirmed?
+u.save!
 company1.build_address(house_no: "67", locality: "kharadi", pincode: 416417, city: "Benglore", state: "karnataka")
 company1.save!
 
-
-company1.employees.find_or_create_by(email: "kirand@dummysoftware.com", name: "Kiran Deshmukh",mobile_number: "+919421215727",is_active:true,role: "employee") do |user|
-  user.password = "kiran123"
-  user.confirmed_at = Time.now
+10.times do |i|
+  u = company1.employees.find_or_create_by(name: "test#{i}", email: "test#{i}@test.com}", mobile_number: "987654456#{i}", is_active: true, role: "employee")
+  u.password = "test@#{i}"
+  u.confirmed_at = Time.now if !u.confirmed?
+  u.save!
 end
-company1.employees.find_or_create_by(name: 'Akshay Kakade', mobile_number: "+919096089881",
- email: "akshay@dummysoftware.com", is_active: true, role: "employee") do |user|                
-  user.password = "akshay123"
-  user.confirmed_at = Time.now
-end
-company1.employees.find_or_create_by(name: 'Shivam Singh', mobile_number: "+917746991695",
- email:  "shivam@dummysoftware.com", is_active: true, role: "employee") do |user|
-  user.password = "shivam123" 
-  user.confirmed_at = Time.now 
-end
-
-
-
-
-
-
-
-
-
-

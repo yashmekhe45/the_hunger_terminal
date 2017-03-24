365400e4c721b104cfb4a9fc07fb157a422838e5
tejaswini-gambhire <gambhire.tejaswini30@gmail.com>
2017-03-24 22:32:49 +0530

    Sends mail to the terminal[#69]

--- app/controllers/custom_actions_controller.rb
+++ app/controllers/custom_actions_controller.rb
@@ -9,6 +9,13 @@ class CustomActionsController < ApplicationController
   end
 
   def confirm
-    @order = Order.menu_details(params[:terminal_id],current_user.company_id)
+    @orders = Order.menu_details(params[:terminal_id],current_user.company_id)
+  end
+
+  def place_orders
+    OrderMailer.send_mail_to_terminal(params[:terminal_id], current_user.company_id).deliver_now
+    flash[:notice] = "email sent successfully"
+    Order.update_status(params[:terminal_id], current_user.company_id)
+    redirect_to custom_actions_index_path
   end
 end
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,2 +1,10 @@
 class OrderMailer < ApplicationMailer
+  default from: ENV["GMAIL_USERNAME"]
+
+  def send_mail_to_terminal(terminal_id, company_id)
+    @terminal = Terminal.find(terminal_id)
+    @orders = Order.menu_details(terminal_id, company_id)
+    mail(to: @terminal.email, subject: 'orders for today')
+  end
+
 end

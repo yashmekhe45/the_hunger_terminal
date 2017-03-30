6b22dd7aaf1fb78fbbdd335acfba135d7a375bce
kakadeakshay <kakadeakshaykumar@gmail.com>
2017-03-30 13:18:21 +0530

    terminal email validation,unwanted spacec removed from name [#68]

--- app/models/terminal.rb
+++ app/models/terminal.rb
@@ -1,14 +1,28 @@
 class Terminal < ApplicationRecord
   
   validates_with LandlineValidator
+  validates_presence_of :email, message: "Emailid cant be blank"
   validates :name, :landline ,presence: true
   validates :landline ,uniqueness: { scope: :company_id }
   validates :landline ,length: { is: 10 }
+  validates_format_of :email,with: Devise.email_regexp, message: "Invalid email format."
   has_many :menu_items, dependent: :destroy
   has_many :orders
   belongs_to :company
 
   mount_uploader :image, ImageUploader
+
+  before_validation :remove_space
+  
+  private 
+    
+  def remove_space
+    #squish method is not for nil classes
+    if(self.name == nil)
+      return
+    end
+    self.name = name.squish
+  end
 
   def self.daily_terminals(c_id)
     self.

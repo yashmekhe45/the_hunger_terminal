a4ff0c29067aec3599eeef92a88213f64b196396
tejaswini-gambhire <gambhire.tejaswini30@gmail.com>
2017-04-01 08:44:44 +0530

    some modification in schema[#69]

--- db/schema.rb
+++ db/schema.rb
@@ -86,9 +86,9 @@ ActiveRecord::Schema.define(version: 20170322104724) do
     t.string   "landline"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.integer  "company_id"
     t.string   "email"
     t.boolean  "active",           default: true
-    t.integer  "company_id"
     t.string   "image"
     t.float    "min_order_amount"
     t.index ["company_id"], name: "index_terminals_on_company_id", using: :btree

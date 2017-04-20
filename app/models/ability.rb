0e8b80739f9d63608c1cc201cd8fe71816a89106
kakadeakshay <kakadeakshaykumar@gmail.com>
2017-04-20 10:32:03 +0530

    can can configuration tax field for vat

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -25,7 +25,7 @@ class Ability
       can :confirm_orders, :order_management
       can [:read, :update, :get_order_details], Company
       cannot [:index, :delete], Company
-      can :manage, Report
+      # can :manage, Report
     end
 
     #

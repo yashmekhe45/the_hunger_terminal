ffcc615e88f082e5f6ad1c00f8f3d041bbe5c2e0
kiran123 <kiranbdesh123@gmail.com>
2017-05-02 09:58:47 +0530

    adds new routes and modifies redundent routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   
-  resources :terminals do
-    resources :orders
-  end
+  root to: 'home#index'
 
   resources :order_details, only: :destroy
 
-  get 'home/index'
   get 'order/vendors' => 'orders#load_terminal' ,:as => 'vendors'
   get 'order/myOrder' => 'orders#order_history' ,:as => 'orders'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -14,35 +11,41 @@ Rails.application.routes.draw do
   as :user do
     get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
     patch 'users' => 'devise/registrations#update', :as => 'user_registration'
-  end  
+  end 
+
+
   resources :companies do
-    get 'get_order_details', :on => :member
+    member do
+      get 'get_order_details'
+    end
+
     resources :users do
       collection do
         get 'search'
         get 'download_invalid_csv'
+        post 'add_multiple_employee_records'
         post 'import'
       end
     end
-  end
 
-  resources :companies do
     resources :terminals do
       resources :menu_items do
-        collection do 
-          post :import 
+        collection do
+          post :import
         end
-      # member { get :download }
-      end 
-    end  
-  end  
+      end
 
-  resources :companies
+      resources :orders
+    end
+  end
+
   
   # get 'companies/:company_id/terminals/:id/invalid_menu_download' => 'terminals#invalid_menu_download'
  
   get "menu_items/download_csv"
   get 'users/download_sample_csv'
+  get 'users/download_sample_xls'
+  get 'users/download_sample_xlsx'
 
   get 'admin_dashboard/index'
   get 'admin_dashboard/order_detail'
@@ -64,6 +67,6 @@ Rails.application.routes.draw do
   get 'reports/individual_terminal_last_month_report'
   get 'reports/employees_daily_order_detail'
   get 'reports/download_daily_terminal_report'
-  root to: 'home#index'
+  
   
 end

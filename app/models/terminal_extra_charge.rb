2da5377908ba1e3b51713f4b1587c78cedd7e54b
kiran123 <kiranbdesh123@gmail.com>
2017-05-05 16:56:06 +0530

    adds extra column for extra charges in order table
     We are maintaining extra charges per order using a separate column
     instead of directly adding in total cost of order

--- app/models/terminal_extra_charge.rb
+++ app/models/terminal_extra_charge.rb
@@ -12,7 +12,7 @@ class TerminalExtraCharge < ApplicationRecord
     @todays_orders = Order.get_terminal_specific_orders(terminal_id, company_id)
     todays_user_count = @todays_orders.to_a.count
     extra_charge_per_employee = daily_extra_charge / todays_user_count
-    @todays_orders.update_all("total_cost = total_cost + #{extra_charge_per_employee}")
+    @todays_orders.update_all("extra_charges =  #{extra_charge_per_employee}")
   end
 
 end

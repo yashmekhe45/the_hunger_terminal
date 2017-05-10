037947a1b068b1cedba4644b676632dfae2bfad8
kiran123 <kiranbdesh123@gmail.com>
2017-05-10 16:23:50 +0530

    changes smtp address for SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
     user_name:      ENV["SENDGRID_USERNAME"],
     password:       ENV["SENDGRID_PASSWORD"],
     domain:         'heroku.com',
-    address:       'smtp.gmail.com',
+    address:      'smtp.sendgrid.net',
     port:          '587',
     authentication: :plain,
     enable_starttls_auto: true

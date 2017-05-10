a957cc7ff53e4ffea91ad7c1ad061632706d0c5b
kiran123 <kiranbdesh123@gmail.com>
2017-05-10 16:05:09 +0530

    changes production smtp settings to use SendGrid credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,8 +93,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    user_name:      ENV["GMAIL_USERNAME"],
-    password:       ENV["GMAIL_PASSWORD"],
+    user_name:      ENV["SENDGRID_USERNAME"],
+    password:       ENV["SENDGRID_PASSWORD"],
     domain:         'heroku.com',
     address:       'smtp.gmail.com',
     port:          '587',

1ce0566228174330940e8a0b1642ff434dae0f49
kiran123 <kiranbdesh123@gmail.com>
2017-05-10 18:46:58 +0530

    modifies default "to" to " reply_to"

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "hunger-terminal@joshsoftware.com"
-  default to: "kiranbdesh123@gmail.com"
+          reply_to: "kiranbdesh123@gmail.com"
   layout 'mailer'
 end

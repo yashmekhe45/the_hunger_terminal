33d4a0202e2ef643ec4b62dbdd33f24ca97bdc16
kakadeakshay <kakadeakshaykumar@gmail.com>
2017-06-09 17:03:28 +0530

    veg field of menu item must be boolean [#142]

--- test/models/menu_item_test.rb
+++ test/models/menu_item_test.rb
@@ -1,47 +1,52 @@
 require "test_helper"
 class MenuItemTest < ActiveSupport::TestCase
   test "should not save without item name" do
-    menuitem = build(:menu_item, :name=>"")
-    menuitem.valid?
-    assert_not_empty menuitem.errors[:name]
+    @menu_item = build(:menu_item, :name=>"")
+    @menu_item.valid?
+    assert_not_empty @menu_item.errors[:name]
   end
 
   test "should not save without veg/nonveg specified" do
-    menuitem = build(:menu_item, :veg=>"")
-    menuitem.valid?
-    assert_not_empty menuitem.errors[:veg]
+    @menu_item = build(:menu_item, :veg=>"")
+    @menu_item.valid?
+    assert_not_empty @menu_item.errors[:veg]
   end
 
   test "should not save without showing price" do
-    menuitem = build(:menu_item, :price=>"")
-    menuitem.valid?
-    assert_not_empty menuitem.errors[:price]
+    @menu_item = build(:menu_item, :price=>"")
+    @menu_item.valid?
+    assert_not_empty @menu_item.errors[:price]
   end
 
   test "should not have price less than zero" do
-    menuitem = build(:menu_item, :price=>0)
-    menuitem.valid?
-    assert_not_empty menuitem.errors[:price]
+    @menu_item = build(:menu_item, :price=>0)
+    @menu_item.valid?
+    assert_not_empty @menu_item.errors[:price]
   end
 
   test "menu item name should be case insensetive" do
-    menuitem = build(:menu_item, :name=>"vada")
-    # menuitem_2 = MenuItem.create(:name=>"VADA",:veg=>true,:price=>150,:terminal_id=>1)
-    menuitem_2 = build(:menu_item, :name=>"VADA")
-    menuitem.save!
-    menuitem_2.valid?
-    assert_not_nil menuitem_2.errors[:name]
+    @menu_item = build(:menu_item, :name=>"vada")
+    @menu_item_2 = build(:menu_item, :name=>"VADA")
+    @menu_item.save!
+    @menu_item_2.valid?
+    assert_not_nil @menu_item_2.errors[:name]
   end
 
   test "active days must present for menu item" do
-    menuitem = build(:menu_item, :active_days => [])
-    refute menuitem.valid?
-    assert_not_empty menuitem.errors[:active_days]
+    @menu_item = build(:menu_item, :active_days => [])
+    refute @menu_item.valid?
+    assert_not_empty @menu_item.errors[:active_days]
   end
 
-  test "menu item available must be boolean" do
-    menuitem = build(:menu_item, available: "always")
-    refute menuitem.valid?
-    assert_not_nil menuitem.errors[:available]
+  test "available field must be boolean" do
+    @menu_item = build(:menu_item, available: "always")
+    refute @menu_item.valid?
+    assert_not_nil @menu_item.errors[:available]
+  end
+
+  test "veg field must be boolean" do
+    @menu_item = build(:menu_item, veg: "not boolean")
+    refute @menu_item.valid?
+    assert_not_nil @menu_item.errors[:veg]
   end
 end

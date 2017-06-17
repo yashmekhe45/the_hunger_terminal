07d4eb9a25edbc0d9e8f0b9efccafdef1c447744
akshay <akshay@joshsoftware.com>
2017-06-17 14:45:22 +0530

    changes factory for company [#142]

--- test/factories/companies.rb
+++ test/factories/companies.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :company do 
     name {Faker::Company.name}
-    landline {"0#{Faker::Number.number(11)}"}
+    landline {"0#{Faker::Number.number(10)}"}
     subsidy {50}
     start_ordering_at { Time.parse "12 AM"}
     review_ordering_at { Time.parse "11:30 AM"}

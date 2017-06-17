3cfca91fedb86b780c4fb2d1429de9c116a2b95a
akshay <akshay@joshsoftware.com>
2017-06-17 14:58:24 +0530

    modifies code usabality using before [#142]

--- test/models/menu_item_test.rb
+++ test/models/menu_item_test.rb
@@ -1,52 +1,61 @@
 require "test_helper"
 class MenuItemTest < ActiveSupport::TestCase
+
+  before :each do
+    @terminal = build(:terminal)
+    @terminal.save
+    @dummy_menu_item = build(:menu_item)
+    @menu_item = @terminal.menu_items.build(build(:menu_item).as_json)
+  end
+
   test "should not save without item name" do
-    @menu_item = build(:menu_item, :name=>"")
+    @menu_item.name = nil
     @menu_item.valid?
     assert_not_empty @menu_item.errors[:name]
   end
 
   test "should not save without veg/nonveg specified" do
-    @menu_item = build(:menu_item, :veg=>"")
-    @menu_item.valid?
+    @menu_item.veg = ""
+    refute @menu_item.valid?
     assert_not_empty @menu_item.errors[:veg]
   end
 
   test "should not save without showing price" do
-    @menu_item = build(:menu_item, :price=>"")
-    @menu_item.valid?
+    @menu_item.price = ""
+    refute @menu_item.valid?
     assert_not_empty @menu_item.errors[:price]
   end
 
   test "should not have price less than zero" do
-    @menu_item = build(:menu_item, :price=>0)
-    @menu_item.valid?
+    @menu_item.price = 0
+    refute @menu_item.valid?
     assert_not_empty @menu_item.errors[:price]
   end
 
   test "menu item name should be case insensetive" do
-    @menu_item = build(:menu_item, :name=>"vada")
-    @menu_item_2 = build(:menu_item, :name=>"VADA")
+    @menu_item.name = "vada"
+    @menu_item_2 = @terminal.menu_items.build(build(:menu_item, :name=>"VADA").as_json)
     @menu_item.save!
-    @menu_item_2.valid?
-    assert_not_nil @menu_item_2.errors[:name]
+    refute @menu_item_2.valid?  
+    assert_not_empty @menu_item_2.errors[:name]
   end
 
   test "active days must present for menu item" do
-    @menu_item = build(:menu_item, :active_days => [])
+    @menu_item.active_days = []
     refute @menu_item.valid?
     assert_not_empty @menu_item.errors[:active_days]
   end
 
   test "available field must be boolean" do
-    @menu_item = build(:menu_item, available: "always")
+    @menu_item.available = "always"
     refute @menu_item.valid?
     assert_not_nil @menu_item.errors[:available]
   end
 
   test "veg field must be boolean" do
-    @menu_item = build(:menu_item, veg: "not boolean")
+    @menu_item.veg = "not boolean"
     refute @menu_item.valid?
     assert_not_nil @menu_item.errors[:veg]
   end
+
 end
--- test/models/terminal_test.rb
+++ test/models/terminal_test.rb
@@ -1,15 +1,20 @@
 require "test_helper"
 class TerminalTest < ActiveSupport::TestCase
+
+  before :each do
+    @terminal = build(:terminal)
+  end
+
   test "should not save terminal without landline" do
-    terminal =  build(:terminal ,:landline =>  nil)
-    terminal.valid?
-    assert_not_empty terminal.errors[:landline]    
+    @terminal.landline =  nil
+    @terminal.valid?
+    assert_not_empty @terminal.errors[:landline]
   end
 
   test "should not save terminal without name" do
-    terminal = build(:terminal,:name => nil)
-    terminal.valid?
-    assert_not_empty terminal.errors[:name]
+    @terminal.name = nil
+    @terminal.valid?
+    assert_not_empty @terminal.errors[:name]
   end
 
   test "should not duplicate landline" do
@@ -21,37 +26,39 @@ class TerminalTest < ActiveSupport::TestCase
   end
 
   test "landline no should have length 11" do
-    terminal = Terminal.create(:name=>"kfc",:landline=>"0121023")
-    terminal.valid?
-    assert_not_empty terminal.errors[:landline]
+    @terminal = Terminal.create(:name=>"kfc",:landline=>"0121023")
+    refute @terminal.valid?
+    assert_not_empty @terminal.errors[:landline]
   end
 
   test "active should accept only boolean" do
-    @terminal = build(:terminal, :active => "not true")
+    @terminal.active = "not true"
     refute @terminal.valid?
     assert_not_empty @terminal.errors[:active]
   end
 
   test "payment made should be positive or zero" do
-    @terminal = build(:terminal, payment_made: -100)
+    @terminal.payment_made = -100
     refute @terminal.valid?
     assert_not_empty @terminal.errors[:payment_made]
   end
 
   test "current_amount should be positive or zero" do
-    @terminal = build(:terminal, current_amount: -100)
+    @terminal.current_amount = -100
     refute @terminal.valid?
     assert_not_empty @terminal.errors[:current_amount]
   end
 
   test "min_order_amount should not negative" do
-    @terminal = build(:terminal, min_order_amount: -100)
+    @terminal.min_order_amount = -100
     refute @terminal.valid?
     assert_not_empty @terminal.errors[:min_order_amount]
   end
 
   test "squish spaces on terminal name" do
-    @terminal = create(:terminal, name: "  dominoz   ")
-    assert_equal @terminal.name, "dominoz"
+    @terminal1 = build(:terminal, name: "  dominoz   ")
+    @terminal1.save
+    assert_equal @terminal1.name, "dominoz"
   end
+
 end

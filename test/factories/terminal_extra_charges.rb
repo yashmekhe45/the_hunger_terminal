d1f0825a149f9226f37ef5c3238dfbdaaf60f2ff
kirandeshmukh <kirand@joshsoftware.com>
2017-06-22 12:58:54 +0530

    adds test case for terminal extra charges model

--- test/factories/terminal_extra_charges.rb
+++ test/factories/terminal_extra_charges.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
   factory :terminal_extra_charge do
+    daily_extra_charge {40}
+    date {Time.zone.today}
+
+    association :company
+    association :terminal
     
   end
 end
--- test/models/terminal_extra_charge_test.rb
+++ test/models/terminal_extra_charge_test.rb
@@ -1,9 +1,36 @@
 require "test_helper"
 
-describe TerminalExtraCharge do
-  let(:terminal_extra_charge) { TerminalExtraCharge.new }
+class TerminalextraChargeTest < ActiveSupport::TestCase
 
-  it "must be valid" do
-    value(terminal_extra_charge).must_be :valid?
+  test "employeewise extra charge should be divided" do
+    @terminal = create(:terminal)
+    @company = @terminal.company
+    create_employees
+    create_orders
+    confirm_orders
+    terminal_extra_charge = create(:terminal_extra_charge, company: @company, terminal: @terminal)
+    #Get updated orders 
+    order1 = Order.find(@order1.id)
+    order2 = Order.find(@order2.id)
+
+    employees_extra_charge = order1.extra_charges + order2.extra_charges
+    extra_charge = terminal_extra_charge.daily_extra_charge
+
+    assert_equal  employees_extra_charge, extra_charge
+  end
+
+  def create_employees
+    @user1 = create(:user, company: @company)
+    @user2 = create(:user, company: @company)
+  end
+
+  def create_orders
+    @order1 = create(:order, company: @company, terminal: @terminal, user: @user1)
+    @order2 = create(:order, company: @company, terminal: @terminal, user: @user2)
+  end
+
+  def confirm_orders
+    @order1.update_attribute(:status, "confirmed")
+    @order2.update_attribute(:status, "confirmed")
   end
 end

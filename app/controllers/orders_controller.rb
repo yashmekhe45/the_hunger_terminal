c2a86293edcd6647efbaf731ca25d9c185901497
kirandeshmukh <kirand@joshsoftware.com>
2017-06-28 14:40:29 +0530

    minor fix: order discount[#163]
    Editing order will have discount which is at the time of placing the
    order.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -54,7 +54,7 @@ class OrdersController < ApplicationController
 
 
   def edit
-    @subsidy = current_user.company.subsidy
+    @subsidy = @order.discount
     @order_details = @order.order_details.all.includes(:menu_item) 
     oder_menus = @order.order_details.pluck(:menu_item_id)
     terminal_menus = @terminal.menu_items.pluck(:id)

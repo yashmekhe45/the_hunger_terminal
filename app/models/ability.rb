6b7118fdfcd100680afd51d3a736f0e05bcc4a55
meenakshikumari <meenakshi@joshsoftware.com>
2017-07-24 12:48:25 +0530

    Minor fix in cancancan and edit form of terminal [#170]
    - cancancan problem solved for company sign_up form
    - edit form of terminal was not getting terminal id which was fixed

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -18,11 +18,14 @@ class Ability
       cannot :payment, :payment_management
     end
 
+    
+    
     if user.role == "company_admin"
       can :manage, Order
       cannot [:edit, :delete], Order, status: ['placed','confirmed','cancelled']
+      can :manage, User, :company_id => user.company_id
       # company_admin won't be allowed to access the company index
-      can :manage, Company, :id => user.company_id 
+      can [:read, :update, :get_order_details, :download_invalid_csv], Company, id: user.company_id
       can :manage, Terminal, :company_id => user.company_id
       can :manage, User, :company_id => user.company_id
       can :manage, MenuItem, :company_id => user.company_id

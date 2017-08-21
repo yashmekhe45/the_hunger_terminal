32f47456bcbccffd4a587ad9d0c995a477a4050a
meenakshikumari <meenakshi@joshsoftware.com>
2017-08-21 18:19:09 +0530

    [#193]Replaced inline by sidekiq

--- app/jobs/send_order_mail_job.rb
+++ app/jobs/send_order_mail_job.rb
@@ -1,7 +1,12 @@
 class SendOrderMailJob < ApplicationJob
   queue_as :default
 
-  def perform(terminal_id, orders, message, company_id)
-    OrderMailer.send_mail_to_terminal(terminal_id, orders, message, company_id).deliver_later
+  # def perform(terminal_id, orders, message, company_id)
+  #   # OrderMailer.send_mail_to_terminal(terminal_id, orders, message, company_id).deliver_later
+  # end
+
+  def perform(employee)
+    OrderMailer.send_mail_to_employees(employee).deliver_later
   end
+  
 end

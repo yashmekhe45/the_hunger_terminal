2c5f91c3999f86ceab8769290d778445fea051ab
meenakshikumari <meenakshi@joshsoftware.com>
2017-09-05 18:13:32 +0530

    [#117]Rounded up the CTE in reports
    - rounded up CTE to nearest tens digit in monthly employee reports
    - written test cases for reports_helper

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,13 @@
 module ReportsHelper
+
+  def round_of_total(last_month_total)
+    # byebug
+    num = last_month_total % 10
+    last_month_total = (num >= 3)?
+                      ((last_month_total/10).floor+1) * 10
+                       : (last_month_total/10).floor * 10
+    p "₹ " + "#{last_month_total}"
+  end
+
+  
 end

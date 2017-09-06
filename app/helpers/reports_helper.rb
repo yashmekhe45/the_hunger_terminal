54f55e738829a77272d8eef0f3cb44611314b8a9
meenakshikumari <meenakshi@joshsoftware.com>
2017-09-06 18:58:56 +0530

    [#117]Column-width for pdf file corrected
    - Monthly_report_of_employee pdf column-width adjusted in @media print in
      application.scss

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,7 +1,6 @@
 module ReportsHelper
 
   def round_of_total(last_month_total)
-    # byebug
     num = last_month_total % 10
     last_month_total = (num >= 3)?
                       ((last_month_total/10).floor+1) * 10

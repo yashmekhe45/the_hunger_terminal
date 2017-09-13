5bf9c661b9be305eaf5bd1a917403262f2181918
kirandeshmukh <kirand@joshsoftware.com>
2017-09-13 23:23:33 +0530

    [#125]Move queries to models
    - Modified query for retrieving last three distinct orders

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -15,4 +15,29 @@ class OrderDetail < ApplicationRecord
     self.price = menu_item.price  
     self.status = 'available' 
   end
+
+  def self.get_orders_for_one_click_ordering(employee_id)
+    orders = order_ids = []
+    #order_details is the array of hashes where key: OrderId and value: Array of arrays having menu_item_name and quantity
+    order_details = OrderDetail.get_order_details(employee_id)
+    unless order_details.empty?
+      order_details.uniq!(&:values)
+      order_details.take(3).each{|order_hash|  order_ids << order_hash.keys }
+      order_ids.flatten!
+      orders = OrderDetail.get_orders(order_ids)  
+    end
+    orders
+  end
+
+  private 
+
+  def self.get_order_details(employee_id)
+    self.joins(:order).select("DISTINCT ON (order_id, orders.terminal_id, menu_item_id, quantity) *,orders.terminal_id AS terminal_id").where("orders.user_id" =>"#{employee_id}","orders.status" => 'confirmed').order('order_id desc').group_by(&:order_id).map {|order_id, order_detail| { order_id => order_detail.pluck(:menu_item_name, :quantity, :terminal_id)}}
+  end
+
+  def self.get_orders(order_ids)
+    orders = []
+    order_ids.each{ |order_id| orders << Order.find(order_id)}
+    orders
+  end
 end

41f67ee348798229cd24a813ec397f214ec36c7d
Raj Kamal Lashkari <raj@joshsoftware.com>
2018-07-31 17:32:31 +0530

    Code refactoring

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,7 @@
 module OrdersHelper
+
+  def min_order_reached?(terminal)
+    terminal.min_order_amount <= terminal.ordered_amount
+  end
+
 end
--- app/models/terminal.rb
+++ app/models/terminal.rb
@@ -23,7 +23,8 @@ class Terminal < ApplicationRecord
   before_save :titleize_name
 
   def ordered_amount
-    Order.where(status: 'pending',terminal_id: id).sum(:total_cost)
+    Order.where(status: 'pending',terminal_id: id, date: Time.zone.today)
+         .sum(:total_cost)
   end
 
   def confirmation_possibility

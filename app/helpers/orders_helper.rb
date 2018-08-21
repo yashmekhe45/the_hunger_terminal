8071cd8c6deb1dbe51f7923ab3de0e64f0863955
Raj Kamal Lashkari <raj@joshsoftware.com>
2018-08-21 17:44:54 +0530

    Showing average rating of terminals in stars

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,5 +1,17 @@
 module OrdersHelper
 
+  def full_star(rating)
+    rating.round / 2
+  end
+
+  def half_star?(rating)
+    rating.round % 2 == 1
+  end
+
+  def empty_star(rating)
+    (5 - (rating.round / 2) - rating % 2).round
+  end
+
   def min_order_reached?(terminal, order_id = nil)
     return true if terminal.min_order_amount == 0
     terminal.min_order_amount <= terminal.ordered_amount(order_id)
